90bf4ef7108de48e60a9245cd9add0caedf13566
Chuck Rolke <crolke@redhat.com>
2017-07-21 17:33:20 -0400

    Update docker build; update version on submission form
    Have docker build reuse some cached images but not all.

--- docker/build-this.sh
+++ docker/build-this.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-docker build -t local:amqp-adverb .
+docker build -t local:amqp-adverb --build-arg CACHEBUST=$(date +%s) .
+
+# then to publish:
+#
+# docker images
+#  <find the IMAGE ID of the fresh build>
+# docker tag <theId> chugr/amqp-adverb
+# export DOCKER_ID_USER="chugr"
+# docker login
+# docker push chugr/amqp-adverb
